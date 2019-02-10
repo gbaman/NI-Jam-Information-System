fef4956dc603928bb647b569deab4c450e053ba8
gbaman <gbaman@users.noreply.github.com>
2019-02-10 16:33:52 +0000

    Add new digital badges module

--- ni_jam_information_system/decorators.py
+++ ni_jam_information_system/decorators.py
@@ -136,3 +136,11 @@ def module_equipment_required(f):
         return _module_required(f, configuration.Modules.module_equipment, *args, **kwargs)
 
     return decorated_function
+
+
+def module_badge_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        return _module_required(f, configuration.Modules.module_badge, *args, **kwargs)
+
+    return decorated_function
