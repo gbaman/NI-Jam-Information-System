e60bcd298967a0911af130fb27a58d4cb53042f5
gbaman <gbaman@users.noreply.github.com>
2019-03-07 23:44:44 +0000

    Switch superadmin to level 5 and add new level 4 permission of trustee

--- ni_jam_information_system/decorators.py
+++ ni_jam_information_system/decorators.py
@@ -8,6 +8,23 @@ import configuration
 def super_admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        login_status, user = logins.check_allowed(request, 5) # Check if user (via cookie) is allowed to access the page
+        request.logged_in_user = user
+        if login_status: # If allowed to access the page
+            return f(*args, **kwargs)
+
+        if user: # If logged in, but not allowed to access the page
+            return redirect("505")
+
+        flash("You do not currently have permission to access the requested page. Please log in first.", "danger")
+        return redirect(url_for('public_routes.login', next=request.url)) # If not logged in
+
+    return decorated_function
+
+
+def trustee_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
         login_status, user = logins.check_allowed(request, 4) # Check if user (via cookie) is allowed to access the page
         request.logged_in_user = user
         if login_status: # If allowed to access the page
