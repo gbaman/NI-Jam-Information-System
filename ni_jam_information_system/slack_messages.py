f926b49d88b4df74455a618e083ceadabe614d3d
Andrew Mulholland <gbaman@users.noreply.github.com>
2023-01-10 21:43:41 +0000

    Add better slack message error catching

--- ni_jam_information_system/slack_messages.py
+++ ni_jam_information_system/slack_messages.py
@@ -1,4 +1,6 @@
 import slack
+import traceback
+import slack.errors
 
 import configuration
 from secrets import config
@@ -22,15 +24,18 @@ def _get_user_id_from_email(users, email):
 def send_slack_direct_message(users: List[models.LoginUser], message):
     if not configuration.verify_modules_enabled().module_slack:
         return
-    client = _setup_slack_client()
-    slack_users = client.users_list()["members"]
-    for user in users:
-        user_id = _get_user_id_from_email(slack_users, user.email)
-        if user_id:
-            # c = client.conversations_open(users=[user_id]) # Not sure if this is actually needed anymore?
-            try:
-                client.chat_postMessage(channel=user_id, text=message, as_user="true")
-                print(f"Slack Message sent to {user.first_name} {user.surname} with message of \"{message[:40]}\"...")
-            except Exception as e:
-                print(e)
-                print(f"Unable to send Slack message to {user.first_name} {user.surname}.")
+    try:
+        client = _setup_slack_client()
+        slack_users = client.users_list()["members"]
+        for user in users:
+            user_id = _get_user_id_from_email(slack_users, user.email)
+            if user_id:
+                # c = client.conversations_open(users=[user_id]) # Not sure if this is actually needed anymore?
+                try:
+                    client.chat_postMessage(channel=user_id, text=message, as_user="true")
+                    print(f"Slack Message sent to {user.first_name} {user.surname} with message of \"{message[:40]}\"...")
+                except Exception as e:
+                    print(e)
+                    print(f"Unable to send Slack message to {user.first_name} {user.surname}.")
+    except slack.errors.SlackApiError:
+        traceback.print_exc()
