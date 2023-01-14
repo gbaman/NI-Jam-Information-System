3f0cf57d615e5e844d63b64bfbd172b6a713dba1
Andrew Mulholland <gbaman@users.noreply.github.com>
2023-01-14 23:18:25 +0000

    Add slack notify for specific selected Jam ID

--- ni_jam_information_system/routes/trustee_routes.py
+++ ni_jam_information_system/routes/trustee_routes.py
@@ -298,15 +298,15 @@ def police_checks_admin_verify_all():
     return redirect(misc.redirect_url())
 
 
-@trustee_routes.route("/volunteer_attendance_remind_slack")
+@trustee_routes.route("/volunteer_attendance_remind_slack/<jam_id>")
 @trustee_required
 @module_slack_required
 @module_volunteer_attendance_required
-def volunteer_attendance_remind_slack():
+def volunteer_attendance_remind_slack(jam_id):
     user_triggered_by = database.get_user_from_cookie(request.cookies.get('jam_login'))
     users = database.get_users_not_responded_to_attendance(database.get_current_jam_id())
     jam_info = database.get_jam_details(database.get_current_jam_id())
-    slack_messages.send_slack_direct_message(users, f"Please update your Jam attendance for the next Raspberry Jam in {(jam_info.date - datetime.now()).days} days! Click the following link to bring you to the Volunteer attendance screen. {configuration.verify_config_item('general', 'base_url')}/admin/volunteer_attendance -  (Sent by {user_triggered_by.first_name.capitalize()})")
+    slack_messages.send_slack_direct_message(users, f"Please update your Jam attendance for the next Raspberry Jam in {(jam_info.date - datetime.now()).days} days! Click the following link to bring you to the Volunteer attendance screen. {configuration.verify_config_item('general', 'base_url')}/admin/volunteer_attendance/{jam_id} -  (Sent by {user_triggered_by.first_name.capitalize()})")
     database.set_configuration_item("slack_reminder_sent", database.convert_to_mysql_datetime(datetime.now()))
     flash("Notification sent out to all users who have not responded, with a linked Slack account", "success")
     return redirect("/admin/volunteer_attendance")
